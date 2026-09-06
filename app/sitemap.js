75f4ef0f0f1c8ef3d7fffeb9ba643112bfcd5c68
AlexAlstr <jampingkuda2@gmail.com>
2026-09-06 18:27:06 +0700

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  const baseUrl = "https://riohoki.my.id";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/login`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/register`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
